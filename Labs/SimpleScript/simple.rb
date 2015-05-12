723b958de3b23c69671436ed231e5405f0b51685
Chris Beacom <uwlceo@gmail.com>
2015-05-12 17:02:00 -0400

    End of Day 2

--- Labs/SimpleScript/simple.rb
+++ Labs/SimpleScript/simple.rb
@@ -1,8 +1,7 @@
 require 'watir-webdriver'
 
-browser = Watir::Browser.new :chrome
+browser = Watir::Browser.new(:chrome)
 
 browser.goto 'http://manifestsc.com'
 
 browser.a(:text => 'ABOUT US').click
-
