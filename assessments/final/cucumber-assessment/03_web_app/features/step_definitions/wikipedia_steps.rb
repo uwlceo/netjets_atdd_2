fa002436fcf734834ed7784f5564fb055f72ea5a
Chris Beacom <uwlceo@gmail.com>
2015-05-15 15:54:23 -0400

    Almost Done

--- assessments/final/cucumber-assessment/03_web_app/features/step_definitions/wikipedia_steps.rb
+++ assessments/final/cucumber-assessment/03_web_app/features/step_definitions/wikipedia_steps.rb
@@ -2,6 +2,9 @@ require 'locale'
 require 'test/unit'
 require 'page-object'
 
+require 'test/unit/assertions'
+include Test::Unit::Assertions
+
 When(/^I access Wikipedia for my language$/) do
   @browser.goto "http://wikipedia.com"
   language = Locale.current.language
